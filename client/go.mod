8e1c38def1b04b29cefecf2e5f252b8c3cf7f2af
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-03-02 00:03:32 +0100

    feat(client): finalize client

--- client/go.mod
+++ client/go.mod
@@ -1,3 +1,13 @@
 module arsync/client
 
 go 1.22.0
+
+require (
+	github.com/akamensky/argparse v1.4.0
+	github.com/jlaffaye/ftp v0.2.0
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+)
--- client/main.go
+++ client/main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
 	"os"
+
+	"github.com/akamensky/argparse"
+	"github.com/jlaffaye/ftp"
 )
 
 func main() {
-  cmdLineArgs := os.Args
+  parser := argparse.NewParser("client", "Connects to a server and gets a folder")
+  host := parser.String("H", "host", &argparse.Options{Required: true, Help: "Host to connect to"})
+  port := parser.String("P", "port", &argparse.Options{Required: true, Help: "Port to connect to"})
+  folder := parser.String("f", "folder", &argparse.Options{Required: true, Help: "Folder to get"})
+  ftpUsername := parser.String("u", "username", &argparse.Options{Required: true, Help: "FTP username"})
+  ftpPassword := parser.String("p", "password", &argparse.Options{Required: true, Help: "FTP password"})
 
-  if len(cmdLineArgs) < 4 {
-    log.Fatalf("Usage: %s <host> <port> <folder>", cmdLineArgs[0])
+  err := parser.Parse(os.Args)
+
+  if err != nil {
+    log.Fatal(parser.Usage(err))
   }
 
-  host := cmdLineArgs[1]
-  port := cmdLineArgs[2]
-  folder := cmdLineArgs[3]
+  // host := cmdLineArgs[1]
+  // port := cmdLineArgs[2]
+  // folder := cmdLineArgs[3]
+  // ftpUsername := cmdLineArgs[4]
+  // ftpPassword := cmdLineArgs[5]
 
   log.Printf("Connecting to %s:%s and getting %s", host, port, folder)
 
-  addr := net.JoinHostPort(host, port)
+  addr := net.JoinHostPort(*host, *port)
   conn, err := net.Dial("tcp", addr)
 
   if err != nil {
@@ -30,7 +43,7 @@ func main() {
   log.Printf("Connected to %s", addr)
  
   // calculate the length of the folder name
-  folderLen := len(folder)
+  folderLen := len(*folder)
 
   if folderLen > 255 {
     log.Fatalf("Folder name is too long: %s", folder)
@@ -43,7 +56,7 @@ func main() {
     log.Fatalf("Could not send folder length: %s", err)
   }
 
-  _, err = conn.Write([]byte(folder))
+  _, err = conn.Write([]byte(*folder))
 
   if err != nil {
     log.Fatalf("Could not send folder name: %s", err)
@@ -62,4 +75,47 @@ func main() {
   }
 
   log.Printf("Server accepted folder name: %s", folder)
+
+  ftpAddr := net.JoinHostPort(*host, "21")
+  ftpConn, err := ftp.Dial(ftpAddr)
+
+  if err != nil {
+    log.Fatalf("Could not connect to FTP server: %s", err)
+  }
+
+  defer ftpConn.Quit()
+
+  err = ftpConn.Login(*ftpUsername, *ftpPassword)
+
+  if err != nil {
+    log.Fatalf("Could not login to FTP server: %s", err)
+  }
+
+  log.Printf("Logged in to FTP server")
+
+  archiveName := *folder + ".tar.gz"
+
+  file, err := ftpConn.Retr(archiveName)
+
+  if err != nil {
+    log.Fatalf("Could not retrieve file: %s", err)
+  }
+
+  defer file.Close()
+
+  outputFile, err := os.Create(archiveName)
+
+  if err != nil {
+    log.Fatalf("Could not create file: %s", err)
+  }
+
+  defer outputFile.Close()
+
+  _, err = io.Copy(outputFile, file)
+
+  if err != nil {
+    log.Fatalf("Could not write file: %s", err)
+  }
+
+  log.Printf("Wrote file: %s", archiveName)
 }

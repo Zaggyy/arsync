45bdbc2852f45e6cf2ff3312055688eb1ce8f509
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-05-09 00:13:57 +0200

    feat(proto): generate proto code

--- gen_proto.sh
+++ gen_proto.sh
@@ -4,4 +4,4 @@ protoc --go_out=. \
        --go_opt=paths=source_relative \
        --go-grpc_out=. \
        --go-grpc_opt=paths=source_relative \
-       proto/arsync.proto
+       arsync/arsync.proto

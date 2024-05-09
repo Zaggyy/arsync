7d257ad4aeda6b4c5a90c09d047bb777c07deda3
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-05-09 03:25:09 +0200

    feat(client): sleep before exit
    useful for scripting

--- client/main.go
+++ client/main.go
@@ -4,6 +4,7 @@ import (
 	"arsync/arsync"
 	"context"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
@@ -15,6 +16,8 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+const SLEEP_TIME = 5 * time.Second
+
 var (
 	address     = flag.String("address", "localhost:1337", "The address of the Arsync server")
 	folder      = flag.String("folder", "", "The folder to prepare")
@@ -24,40 +27,48 @@ var (
 	ftpPassword = flag.String("ftp-password", "", "The password for the FTP server")
 )
 
+func FatalLogWithSleep(message string, sleep time.Duration) {
+  log.Printf(message)
+  time.Sleep(sleep)
+  os.Exit(1)
+}
+
 func main() {
 	flag.Parse()
 
 	if len(*folder) == 0 {
 		flag.Usage()
-		return
+    time.Sleep(SLEEP_TIME)
+    os.Exit(1)
 	}
 
 	if len(*ftpUsername) == 0 || len(*ftpPassword) == 0 {
-		log.Fatalf("FTP username and password must be provided")
+		FatalLogWithSleep("FTP username and password must be provided", SLEEP_TIME)
 	}
 
 	if len(*username) == 0 || len(*password) == 0 {
-		log.Fatalf("Username and password must be provided")
+    FatalLogWithSleep("Username and password must be provided", SLEEP_TIME)
 	}
 
 	// Create an FTP connection
 	ftpAddr := net.JoinHostPort(*address, "21")
 	ftpConn, err := ftp.Dial(ftpAddr, ftp.DialWithTimeout(time.Second*10))
 	if err != nil {
-		log.Fatalf("Failed to connect to FTP server: %v", err)
+    FatalLogWithSleep(fmt.Sprintf("Failed to connect to the FTP server: %v", err), SLEEP_TIME)
 	}
 	defer ftpConn.Quit()
 
 	// Login to the FTP server
 	err = ftpConn.Login(*ftpUsername, *ftpPassword)
 	if err != nil {
-		log.Fatalf("Failed to login to FTP server: %v", err)
+    FatalLogWithSleep(fmt.Sprintf("Failed to login to the FTP server: %v", err), SLEEP_TIME)
 	}
+  log.Printf("Successfully logged in to the FTP server")
 
 	// Connect to the Arsync server
 	conn, err := grpc.Dial(*address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Failed to connect to Arsync server: %v", err)
+    FatalLogWithSleep(fmt.Sprintf("Failed to connect to Arsync server: %v", err), SLEEP_TIME)
 	}
 	defer conn.Close()
 
@@ -68,11 +79,11 @@ func main() {
 
 	response, err := client.Prepare(ctx, &arsync.PrepareRequest{Path: *folder, Username: *username, Password: *password})
 	if err != nil {
-		log.Fatalf("Failed to prepare folder: %v", err)
+    FatalLogWithSleep(fmt.Sprintf("Failed to prepare folder: %v", err), SLEEP_TIME)
 	}
 
 	if !response.Success {
-		log.Fatalf("Failed to prepare folder: %s", *folder)
+    FatalLogWithSleep(fmt.Sprintf("Failed to prepare folder: %s", *folder), SLEEP_TIME)
 	}
 
 	// Download the zip file
@@ -81,22 +92,23 @@ func main() {
 
 	archiveFile, err := ftpConn.Retr(archiveName)
 	if err != nil {
-		log.Fatalf("Failed to download zip file: %v", err)
+    FatalLogWithSleep(fmt.Sprintf("Failed to download zip file: %v", err), SLEEP_TIME)
 	}
 	defer archiveFile.Close()
 
 	// Create the file
 	file, err := os.Create(archiveName)
 	if err != nil {
-		log.Fatalf("Failed to create file: %v", err)
+    FatalLogWithSleep(fmt.Sprintf("Failed to create file: %v", err), SLEEP_TIME)
 	}
 	defer file.Close()
 
 	// Copy the file
 	_, err = io.Copy(file, archiveFile)
 	if err != nil {
-		log.Fatalf("Failed to copy file: %v", err)
+    FatalLogWithSleep(fmt.Sprintf("Failed to copy file: %v", err), SLEEP_TIME)
 	}
 
 	log.Printf("Successfully downloaded zip file %s", archiveName)
+  time.Sleep(SLEEP_TIME)
 }
